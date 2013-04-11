65743f3763b7372c8c791a754d305aad00d23aa1
makoto_kw <makoto.kw@gmail.com>
2013-04-11 13:23:46 +0900

    Fixed mimetypes for markdown

--- init.rb
+++ init.rb
@@ -9,11 +9,16 @@ Redmine::Plugin.register :redmine_render_markdown_in_repository do
   author_url 'http://makotokw.com'
 end
 
+MARKDOWN_EXTS = %w(md markdown).freeze
+MARKDOWN_EXTS.each do |ext|
+  Redmine::MimeType::EXTENSIONS[ext] = 'text/x-markdown'
+end
+
 RepositoriesController.class_eval do
   alias redmine_render_markdown_in_repository_orig_entry entry
   def entry
     redmine_render_markdown_in_repository_orig_entry
-    if !performed? && @path =~ /\.(?:md|markdown)$/i
+    if !performed? && @path =~ /\.(?:#{MARKDOWN_EXTS.join('|')})$/i
       markdown = Redcarpet::Markdown.new(MarkdownRender,
                                          :tables => true, :fenced_code_blocks => true,
                                          :autolink => true, :space_after_headers => true)
