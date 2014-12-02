f6c09fa787d7fc7708b600df139be635ab708c63
makoto_kw <makoto.kw@gmail.com>
2014-12-02 20:30:04 +0900

    Fixed encoding for Ruby 1.9

--- init.rb
+++ init.rb
@@ -1,29 +1,19 @@
-require 'markdown/render'
+require 'redmine'
+require_dependency 'markdown/render'
+require_dependency 'markdown/repositories_controller_patch'
+
+Rails.configuration.to_prepare do
+  unless RepositoriesController.included_modules.include? MarkdownRepositoriesControllerPatch
+    RepositoriesController.send(:include, MarkdownRepositoriesControllerPatch)
+  end
+end
 
 Redmine::Plugin.register :redmine_render_markdown_in_repository do
   name 'Render Markdown in Repository'
   author 'Makoto Kawasaki'
-  description 'This plugin renders markdown file in repository'
-  version '1.0.0'
-  url 'https://github.com/makotokw/redmine_render_markdown_in_repository'
   author_url 'http://makotokw.com'
-end
-
-MARKDOWN_EXTS = %w{md markdown}.freeze
-MARKDOWN_EXTS.each do |ext|
-  Redmine::MimeType::EXTENSIONS[ext] = 'text/x-markdown'
-end
-
-RepositoriesController.class_eval do
-  alias redmine_render_markdown_in_repository_orig_entry entry
-  def entry
-    redmine_render_markdown_in_repository_orig_entry
-    if !performed? && @path =~ /\.(?:#{MARKDOWN_EXTS.join('|')})$/i
-      markdown = Redcarpet::Markdown.new(MarkdownRender,
-                                         :tables => true, :fenced_code_blocks => true,
-                                         :autolink => true, :space_after_headers => true)
-      @content = markdown.render(@content)
-      render :action => 'entry_markdown'
-    end
-  end
+  description 'This plugin renders markdown file in repository'
+  version '1.0.1'
+  url 'https://github.com/makotokw/redmine_render_markdown_in_repository'
+  requires_redmine :version_or_higher => '2.0'
 end
